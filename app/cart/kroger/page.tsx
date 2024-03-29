95b83c1ade3a2d043a629ad9085e0594cff086b5
Jwest11 <joeywest1414@gmail.com>
2024-03-28 18:35:05 -0600

    fixed folder capitalization

---
+++ app/cart/kroger/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import type { MySession } from "@/types";
+import KrogerCartClient from "@/components/client/krogerCartClient/krogerCartClient";
+import { redirect } from "next/navigation";
+
+export default async function KrogerCart() {
+
+    const session = await getServerSession(authOptions) as MySession;
+    if (!session || !session.accessToken || session.expiresAt < Date.now()) {
+        redirect("/auth/kroger/signin");
+    }
+
+    return (<>
+        <KrogerCartClient></KrogerCartClient>
+    </>
+        
+    )
+}
